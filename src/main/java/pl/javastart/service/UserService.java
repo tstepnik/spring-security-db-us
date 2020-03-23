327c7d4c3eaabf4d55d9fe727bc08e7453d0e8a3
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-23 10:40:03 +0100

    spring boot security app with registration feature

---
+++ src/main/java/pl/javastart/service/UserService.java
@@ -0,0 +1,45 @@
+package pl.javastart.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import pl.javastart.model.User;
+import pl.javastart.model.UserRole;
+import pl.javastart.repository.UserRepository;
+import pl.javastart.repository.UserRoleRepository;
+
+@Service
+public class UserService {
+
+    private static final String DEFAULT_ROLE = "ROLE_USER";
+    private UserRepository userRepository;
+    private UserRoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setRoleRepository(UserRoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public void addWithDefaultRole(User user) {
+        UserRole defaultRole = roleRepository.findByRole(DEFAULT_ROLE);
+        user.getRoles().add(defaultRole);
+        String passwordHash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(passwordHash);
+        userRepository.save(user);
+    }
+}
